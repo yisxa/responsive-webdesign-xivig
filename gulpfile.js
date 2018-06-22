ce468fbd1fec6796837e237dfa14aa159eba7c2e
Cyberabc <yisxaa@gmail.com>
2018-06-22 07:05:26 +0530

    autoprefixer & postcss-simple-vars & postcss-nested packages ware updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,40 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss');
 
-gulp.task('default', function(){
-    
+gulp.task('default', function () {
+
     console.log("my first code in gulp");
-    
+
 });
 
-gulp.task('html', function(){
-    
+gulp.task('html', function () {
+
     console.log("cool html trick");
-    
+
 });
 
-gulp.task('styles', function(){
-    
-    return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
-    
+gulp.task('styles', function () {
+
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+
 });
 
 
-gulp.task('watch', function(){
-    
-    watch('./app/index.html', function(){
-    
+gulp.task('watch', function () {
+
+    watch('./app/index.html', function () {
+
         gulp.start('html');
-    
+
     });
-    
-    watch('./app/assets/styles/**/*.css', function(){
-    
+
+    watch('./app/assets/styles/**/*.css', function () {
+
         gulp.start('styles');
     });
-    
+
 
 });
