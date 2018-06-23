9d0d3f8fb06eda792e296e6ff20374821279ed56
Cyberabc <yisxaa@gmail.com>
2018-06-23 17:52:53 +0530

    mixins added

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,13 +3,14 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nestedcss = require('postcss-nested'),
-    cssImport = require('postcss-import');
+    cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins');
 
 
 gulp.task('styles', function () {
   
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nestedcss, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nestedcss, autoprefixer]))
         .on('error', function(errorInfo){
         	console.log(errorInfo);
         	this.emit('end');
