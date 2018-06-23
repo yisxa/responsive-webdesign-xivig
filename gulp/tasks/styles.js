7a351ab509686cffc8a844c818a6450d2b8a990f
System Administrator <root@CyberApples-iMac.local>
2018-06-23 16:09:17 +0530

    gulp error handler was configured

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -10,6 +10,10 @@ gulp.task('styles', function () {
   
     return gulp.src('./app/assets/styles/styles.css')
         .pipe(postcss([cssImport, cssvars, nestedcss, autoprefixer]))
+        .on('error', function(errorInfo){
+        	console.log(errorInfo);
+        	this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/styles'));
 
 });
