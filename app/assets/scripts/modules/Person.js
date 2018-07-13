bad13843c2d09e9c5ed4eb1d65dcc43d2192671b
CyberWebApp <cyberwebapp@CyberWebApps-iMac.local>
2018-07-14 01:28:39 +0530

    webpack added

--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,9 +1,2 @@
-function Person(fullName, favColor){
-    this.name = fullName;
-    this.favoriteColor = favColor;
-    this.greet = function(){
-        alert("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-    }
-}
 
-module.exports = Person;
+
