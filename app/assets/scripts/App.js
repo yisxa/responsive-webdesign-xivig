24e91dc915f8aa3a304c93c9d2fff10d4da2e3ef
cyberabc <yisxaa@gmail.com>
2018-09-02 17:12:31 +0530

    reveal on scroll is done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 import Modal from './modules/Modal.js';
 import $ from 'jquery';
 var mobileMenu = new MobileMenu();
 var modal = new Modal();
+
+new RevealOnScroll($(".service-item"), "70%");
+new RevealOnScroll($(".testimonial"), "85%");
