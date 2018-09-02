04ab9327bb7780639633a1dca21eb7ba0a143efd
cyberabc <yisxaa@gmail.com>
2018-09-03 01:07:38 +0530

    sticky header with smooth scroll done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal.js';
 import $ from 'jquery';
 var mobileMenu = new MobileMenu();
-var modal = new Modal();
-
 new RevealOnScroll($(".service-item"), "70%");
 new RevealOnScroll($(".testimonial"), "85%");
+
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
+
